ca8011e27de48c1df7d599c96bf7b0384cc67905
Kamal Prasat S <kamalprasats@unicult.club>
2025-12-03 03:40:29 +0530

    Add vite config for multi-page build

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        analytics: resolve(__dirname, 'analytics.html'),
+        bookings: resolve(__dirname, 'bookings.html'),
+        converted: resolve(__dirname, 'converted.html'),
+        courses: resolve(__dirname, 'courses.html'),
+        learners: resolve(__dirname, 'learners.html'),
+        revenue: resolve(__dirname, 'revenue.html'),
+        settings: resolve(__dirname, 'settings.html'),
+        warmLeads: resolve(__dirname, 'warm-leads.html'),
+      },
+    },
+  },
+})
+
